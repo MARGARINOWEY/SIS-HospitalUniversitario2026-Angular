b9b2b6c0b13891360afd4ee5977e8c14d0ad8e0a
CRISTIAN DIEGO GARCIA QUISPE <66848889+MARGARINOWEY@users.noreply.github.com>
2026-05-06 08:26:12 -0400

    Add files via upload
    qwerty

---
+++ src/main/java/uap/edu/bo/sisu/sis_hospital_universitario_sisu_2026/SisHospitalUniversitarioSisu2026Application.java
@@ -0,0 +1,13 @@
+package uap.edu.bo.sisu.sis_hospital_universitario_sisu_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisHospitalUniversitarioSisu2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisHospitalUniversitarioSisu2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/uap/edu/bo/sisu/sis_hospital_universitario_sisu_2026/SisHospitalUniversitarioSisu2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package uap.edu.bo.sisu.sis_hospital_universitario_sisu_2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisHospitalUniversitarioSisu2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
